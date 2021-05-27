c653a1ebae181d62ca1e93b2dfdc2ee7ba666149
malykhinv <malykhinv@yandex.ru>
2021-05-27 19:19:35 +0300

    Replace two position fields in User with one Location
    It is not necessary to store two location fields (latitude, longitude) in User objects. It is better to store Location.

--- app/src/main/java/com/malykhinv/footstepsgeo/User.java
+++ app/src/main/java/com/malykhinv/footstepsgeo/User.java
@@ -1,24 +1,24 @@
 package com.malykhinv.footstepsgeo;
 
+import android.location.Location;
+
 public class User {
 
     public String id;
     public String name;
     public String personalCode;
     public int imageNumber;
-    public double latitude;
-    public double longitude;
+    public Location location;
     public String phoneNumber;
     public long lastLocationTime;
     public int batteryLevel;
 
-    public User(String id, String name, String personalCode, int imageNumber, double latitude, double longitude, String phoneNumber, long lastLocationTime, int batteryLevel) {
+    public User(String id, String name, String personalCode, int imageNumber, Location location, String phoneNumber, long lastLocationTime, int batteryLevel) {
         this.id = id;
         this.name = name;
         this.personalCode = personalCode;
         this.imageNumber = imageNumber;
-        this.latitude = latitude;
-        this.longitude = longitude;
+        this.location = location;
         this.phoneNumber = phoneNumber;
         this.lastLocationTime = lastLocationTime;
         this.batteryLevel = batteryLevel;
